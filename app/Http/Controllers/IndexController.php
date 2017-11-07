ae4cabc0cffa172e690705e7d38643fc63dca45e
D3AH <diego@soportedpd.usac.edu.gt>
2017-11-07 15:29:14 +0000

    Redirect correctly but no sessions

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,7 +6,20 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-	public function index() {
-        return "INDEX@CONTROLLER";
+    public function index(Request $request, $opcion) {
+        switch ($opcion) {
+            case "1":
+                return view('form');
+                break;
+            case "2":
+                return "Seguimiento";
+                break;
+            case "3":
+                return "Wizard Ayuda";
+                break;
+            case "4":
+                return "Que es esto!";
+                break;
+        }
     }
 }
